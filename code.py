60c268a1a1fa0dd0ca1f7a8c6478e808c8dc234a
Leon Will <leon.will@782ac29f-8ad9-4e54-8012-08f8b0e74cca.fritz.box>
2025-05-19 15:35:03 +0200

    5th commit

--- code.py
+++ code.py
@@ -20,7 +20,7 @@ links = [
 
 #print(links)
 
-def lade_spiele_von_fupa(teamArray):        
+def lade_spieleSeiten_von_fussball_de(teamArray):
     spiele = {}
     for mannschaft in teamArray:
         spieleMannschaft = []
@@ -45,49 +45,51 @@ def lade_spiele_von_fupa(teamArray):
         # Jeder Spiel-Container
         for elem in tbody.select("tr"):
             #print(elem)
-            if "row-headline" in elem.get("class", []) or "row-competition" in elem.get("class", []):
-                print("überflüssig")
+            if "row-headline" in elem.get("class", []):
+                None
+            elif "row-competition" in elem.get("class", []):
+                dateWrapper = elem.select_one("td.column-date").get_text(strip=True).split("|")
+                date = dateWrapper[0]
+                time = dateWrapper[1]
+                # Datum + Uhrzeit
+                # Format z.B.: "So., 26.05.2024 15:00"
+                date_clean = date
+                for tag in ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]:
+                    date_clean = date_clean.replace(f"{tag}, ", "")
+                if date_clean == "Heute":
+                    date_clean = datetime.today().strftime("%d.%m.%Y")
+                date_clean = date_clean[:8]
+                dateTime = date_clean + " " + time
+                datum = datetime.strptime(dateTime, "%d.%m.%y %H:%M")
+                print(datum)
             else:
                 match = elem
-                #print(match)
                 try:
-                    """ # Datum + Uhrzeit
-                    date_str = match.select_one("span.sc-lhxcmh-0").get_text(strip=True)
-                    time_str = match.select_one("span.sc-4yququ-1").get_text(strip=True)
-                    # Format z.B.: "So., 26.05.2024 15:00"
-                    date_clean = date_str
-                    for tag in ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]:
-                        date_clean = date_clean.replace(f"{tag}., ", "")
-                    if date_clean == "Heute":
-                        date_clean = datetime.today().strftime("%d.%m.%Y")
-                    dateTime = date_clean + " " + time_str
-                    print(dateTime)
-                    datum = datetime.strptime(dateTime, "%d.%m.%Y %H:%M") """
+                    toGame = match.select_one("td.column-detail")
+                    linkToGame = toGame.select_one("a").get("href",[])
 
+                    res = requests.get(linkToGame, headers=headers)
+                    if res.status_code != 200:
+                        print(f"Fehler beim Laden der Seite: {res.status_code}")
+                        return []
+
+                    matchSite = BeautifulSoup(res.text, "html.parser")
                     # Gegner
-                    teams = []
-                    counter = 0
-                    for teamsRaw in match.select("td.column-club"):
-                        counter += 1
-                        team = teamsRaw.select_one("div.club-name")
-                        team_name = clean_text(team.get_text(strip=True))
-                        if len(team) > 2:
-                            team_name += " " + team[2].get_text(strip=True)
-                        teams.append(team_name)
-                        if team_name != mannschaft["teamname"]:
-                            gegner = team_name
-                            if(counter == 1): 
-                                ort = "Auswärtsspiel"
-                            else:
-                                ort = "Heimspiel"
-                    print(teams)
-                    # Ort (falls vorhanden)
-                    #ort_div = match.select_one("div.MatchRow_location__wJkQD")
-                    #ort = ort_div.get_text(strip=True) if ort_div else "Ort unbekannt"
+                    for matchTeams in matchSite.select("div.team-name"):
+                        matchTeam = clean_text(matchTeams.select_one("a").get_text(strip=True))
+                        if matchTeam != mannschaft["teamname"]:
+                            gegner = matchTeam
+                        print(matchTeam)
+
+                    matchLocation = matchSite.select_one("a.location").get_text(strip=True)
+                    #Google Maps Link:
+                    #matchLocation = matchSite.select_one("a.location").get("href",[])
+                    ort = matchLocation or "Ort unbekannt"
+                    print(ort)
 
                     spieleMannschaft.append({
-                        #"datum": datum.isoformat(),
-                        #"zeit": time_str,
+                        "datum": datum.isoformat(),
+                        "zeit": time,
                         "gegner": gegner,
                         "ort": ort
                     })
@@ -101,7 +103,7 @@ def lade_spiele_von_fupa(teamArray):
     return spiele
 
 # 🟩 Aufruf
-spiele = lade_spiele_von_fupa(links)
+spiele = lade_spieleSeiten_von_fussball_de(links)
 
 # ✅ Ausgabe als JSON-Datei
 with open("spiele.json", "w", encoding="utf-8") as f:
